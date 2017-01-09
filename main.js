493b5b716c8c932658d5151c3415851e144f063a
currymich <currymich@gmail.com>
2017-01-09 05:06:44 -0800

    first draft of big format for index

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ $(document).ready(function(){
     $('header nav ul li').toggleClass('active')
   })
 })
+
+// http://stackoverflow.com/questions/4326845/how-can-i-determine-the-direction-of-a-jquery-scroll-event
+
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       $('header').css('height', '0');
+   } else {
+       $('header').css('height', '3.5em')
+   }
+   lastScrollTop = st;
+});
