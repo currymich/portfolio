68f496343f5fcc7279bbb5633e49086f907c5788
currymich <currymich@gmail.com>
2017-01-09 11:02:19 -0800

    finished about page draft

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#menuButton').on('click', function(event){
-    $('header nav ul li').toggleClass('active')
+    $('header nav ul li').toggleClass('extended')
   })
 })
 
@@ -10,9 +10,12 @@ var lastScrollTop = 0;
 $(window).scroll(function(event){
    var st = $(this).scrollTop();
    if (st > lastScrollTop){
-       $('header').css('height', '0');
+       $('header').css({'height': '0', 'overflow': 'hidden'});
    } else {
-       $('header').css('height', 'auto')
+       $('header').css({'background': 'rgb(255, 255, 255)','height': '3.0em', 'overflow': 'visible'})
    }
    lastScrollTop = st;
+   if (st == 0) {
+     $('header').css({'background': 'transparent'})
+   }
 });
